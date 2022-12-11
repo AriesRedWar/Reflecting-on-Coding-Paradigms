513bceda3e63bb64d9568bba3d173ce81c8313ff
AriesRedWar <philipjbowman5@gmail.com>
2022-12-11 09:13:50 -0500

    reflection questions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-fruits_list = []
+#Q-1 Is one of these coding paradigms "better" than the other? Why or why not?
+#A-1 at the moment I think they both have their own functions that can be better but have not had enough practice to make a call on what one is better
 
-def add_fruit(food):
-    if(food['type'] == 'fruit'):
-        fruits_list.append(food)
+#Q-2 Given the opportunity to work predominantly using either of these coding paradigms, which seems more appealing? Why?
+#A-2 object oriented I have a little bit more understanding of this paradigm so I would have to say Object
 
-add_fruit({ 'name': 'strawberry', 'type': 'fruit'})
-add_fruit({'name': 'pizza', 'type': 'hot'})
-add_fruit({ 'name': 'orange', 'type': 'fruit'})
-print(fruits_list)
+#Q-3 Now being more familiar with these coding paradigms, what tasks/features/pieces of logic would be best handled using functional programming? Object Oriented Programming?
+#A-3 functional gives access to being able to evaluate expressions rather then execution of commands. Object Oriented Programming is more software design around datas and objects to be able to handle mor commands in the program.
+
+#Q-4 Personally, which of these styles takes more work to understand? What should be done in order to deepen understanding related to this paradigm?
+#A-4 functional just getting to work with it and test and do projects will make me understand and work better with it.
